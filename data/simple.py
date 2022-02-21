a2f835449e0cf002aceb13d0facca3d140588587
Sebastian Weber <uwmgy@student.kit.edu>
2022-02-21 15:50:35 +0100

    added ssh example

---
+++ data/simple.py
@@ -0,0 +1,143 @@
+# import the m5 (gem5) library created when gem5 is built
+import m5
+# import all of the SimObjects
+from m5.objects import *
+
+import argparse
+
+parser = argparse.ArgumentParser(description="Simulate a binary with gem5.")
+parser.add_argument("binary", type=str)
+parser.add_argument("parameter", type=str, nargs='*')
+args = parser.parse_args()
+
+class L1Cache(Cache):
+    assoc = 2
+    tag_latency = 2
+    data_latency = 2
+    response_latency = 2
+    mshrs = 4
+    tgts_per_mshr = 20
+    
+    def connectCPU(self, cpu):
+        # need to define this in a base class!
+        raise NotImplementedError
+
+    def connectBus(self, bus):
+        self.mem_side = bus.cpu_side_ports
+    
+class L1ICache(L1Cache):
+    size = '16kB'
+
+    def connectCPU(self, cpu):
+        self.cpu_side = cpu.icache_port
+
+class L1DCache(L1Cache):
+    size = '64kB'
+
+    def connectCPU(self, cpu):
+        self.cpu_side = cpu.dcache_port
+    
+class L2Cache(Cache):
+    size = '256kB'
+    assoc = 8
+    tag_latency = 20
+    data_latency = 20
+    response_latency = 20
+    mshrs = 20
+    tgts_per_mshr = 12
+    
+    def connectCPUSideBus(self, bus):
+        self.cpu_side = bus.mem_side_ports
+
+    def connectMemSideBus(self, bus):
+        self.mem_side = bus.cpu_side_ports
+
+# create the system we are going to simulate
+system = System()
+
+# Set the clock fequency of the system (and all of its children)
+system.clk_domain = SrcClockDomain()
+system.clk_domain.clock = '2400MHz'
+system.clk_domain.voltage_domain = VoltageDomain()
+
+# Set up the system
+system.mem_mode = 'timing'               # Use timing accesses
+system.mem_ranges = [AddrRange('1024MB')] # Create an address range
+
+# Create a simple CPU
+system.cpu = O3CPU()
+system.cpu.function_trace = True
+system.cpu.function_trace_start = 0
+system.cpu.tracer = ExeTracer()
+
+
+# Create a memory bus, a system crossbar, in this case
+system.membus = SystemXBar()
+
+system.cpu.icache = L1ICache()
+system.cpu.dcache = L1DCache()
+system.cpu.icache.connectCPU(system.cpu)
+system.cpu.dcache.connectCPU(system.cpu)
+
+system.l2bus = L2XBar()
+
+system.cpu.icache.connectBus(system.l2bus)
+system.cpu.dcache.connectBus(system.l2bus)
+
+system.l2cache = L2Cache()
+system.l2cache.connectCPUSideBus(system.l2bus)
+system.l2cache.connectMemSideBus(system.membus)
+
+
+
+# create the interrupt controller for the CPU and connect to the membus
+system.cpu.createInterruptController()
+
+# For x86 only, make sure the interrupts are connected to the memory
+# Note: these are directly connected to the memory bus and are not cached
+if m5.defines.buildEnv['TARGET_ISA'] == "x86":
+    system.cpu.interrupts[0].pio = system.membus.mem_side_ports
+    system.cpu.interrupts[0].int_requestor = system.membus.cpu_side_ports
+    system.cpu.interrupts[0].int_responder = system.membus.mem_side_ports
+
+# Create a DDR3 memory controller and connect it to the membus
+system.mem_ctrl = MemCtrl()
+system.mem_ctrl.dram = DDR3_1600_8x8()
+system.mem_ctrl.dram.range = system.mem_ranges[0]
+system.mem_ctrl.port = system.membus.mem_side_ports
+
+# Connect the system up to the membus
+system.system_port = system.membus.cpu_side_ports
+
+# get ISA for the binary to run.
+isa = str(m5.defines.buildEnv['TARGET_ISA']).lower()
+
+# Default to running 'hello', use the compiled ISA to find the binary
+# grab the specific path to the binary
+thispath = os.path.dirname(os.path.realpath(__file__))
+binary = '/usr/local/src/gem5/' + args.binary
+
+system.workload = SEWorkload.init_compatible(binary)
+
+# Create a process for a simple "Hello World" application
+process = Process()
+# Set the command
+# cmd is a list which begins with the executable (like argv)
+cmd = []
+cmd.append(binary)
+for parameter in args.parameter:
+    cmd.append(parameter)
+print(cmd)
+process.cmd = cmd
+# Set the cpu to use the process as its workload and create thread contexts
+system.cpu.workload = process
+system.cpu.createThreads()
+
+# set up the root SimObject and start the simulation
+root = Root(full_system = False, system = system)
+# instantiate all of the objects we've created above
+m5.instantiate()
+
+print("Beginning simulation!")
+exit_event = m5.simulate()
+print('Exiting @ tick %i because %s' % (m5.curTick(), exit_event.getCause()))
